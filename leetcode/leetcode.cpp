9fcff38ea5655f27056cbdd2db1049ce1c78bd99
brentca <joenkin@gmail.com>
2016-07-25 16:25:59 -0400

    Update

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -27,6 +27,32 @@ namespace DP {
 	/*10. Regular Expression Matching end */
 
 
+	/*132. Palindrome Partitioning II (hard)
+	https://leetcode.com/problems/palindrome-partitioning-ii/
+	https://discuss.leetcode.com/topic/2840/my-solution-does-not-need-a-table-for-palindrome-is-it-right-it-uses-only-o-n-space
+	*/
+	class Solution132 {
+	public:
+		int minCut(string s) {
+			int n = s.size();
+			vector<int> cut(n + 1, 0);  // number of cuts for the first k characters
+
+			for (int i = 0; i <= n; i++) 
+				cut[i] = i - 1;
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; i - j >= 0 && i + j < n && s[i - j] == s[i + j]; j++) // odd length palindrome
+					cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j]);
+
+				for (int j = 1; i - j + 1 >= 0 && i + j < n && s[i - j + 1] == s[i + j]; j++) // even length palindrome
+					cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j + 1]);
+			}
+			return cut[n];
+		}
+	};
+	/*132. Palindrome Partitioning II end */
+
+
 	/*123. Best Time to Buy and Sell Stock III (hard)
 	https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
 	https://discuss.leetcode.com/topic/5934/is-it-best-solution-with-o-n-o-1
@@ -308,7 +334,7 @@ namespace DP {
 			t[s][e] = res;
 			return res;
 		}
-	}
+	};
 		
 	/*375. Guess Number Higher or Lower II end */
 
